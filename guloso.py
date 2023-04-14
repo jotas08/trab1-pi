4a7a5094434e5f02c127b0b8a444dc49959c15a3
jotinhas <jotasu.drive@gmail.com>
2023-04-13 21:46:18 -0300

    commit inicial com criacao dos modulos e funcoes necessarias

---
+++ guloso.py
@@ -0,0 +1,16 @@
+
+def greedy_algorithm(conj_s, f):
+    c = []
+    u = conj_s
+    while(u != 0):
+        # S* in argmax{|Si|:Si in F}
+        c.append(conj_s*)
+        f.pop(s*)
+        u.pop(s*)
+        for sj in f:
+            #sj = sj - s*
+            if sj = 0:
+                f.pop(sj)
+
+    
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from guloso import greedy_algorithm
+if __name__ == '__main__':
+    print('Loading...')
+    # preprocessamento
+    # algoritmo guloso
+
+    # greedy_algorithm()
